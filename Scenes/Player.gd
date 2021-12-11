ed001d78ff8d0bb058653b24a9ef52798d68c67f
Caezar <63989224+TheOneCaezar@users.noreply.github.com>
2021-12-11 22:42:17 +0100

    Deathscreen added
    deathscreen

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -5,9 +5,15 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 var health=100.0
+var speed = 500
+var attackSpeed = 2
 var is_master = false
+<<<<<<< Updated upstream
 var speed = 700
 var AttackSpeed = 2
+=======
+var dead = false
+>>>>>>> Stashed changes
 signal ability(slot)
 # Called when the node enters the scene tree for the first time.
 func initialize(id):
@@ -24,6 +30,7 @@ func _process(delta):
 	if is_master:
 		var velocity = Vector2()
 		if Input.is_action_pressed("up"):
+<<<<<<< Updated upstream
 			velocity.y = -1
 		if Input.is_action_pressed("down"):
 			velocity.y = 1
@@ -38,9 +45,32 @@ func _process(delta):
 		if Input.is_action_just_pressed("ability3"):
 			emit_signal("ability", 3)
 		velocity=velocity.normalized()*speed
+=======
+			velocity.y = -speed
+		if Input.is_action_pressed("down"):
+			velocity.y = speed
+		if Input.is_action_pressed("left"):
+				velocity.x = -speed
+		if Input.is_action_pressed("right"):
+				velocity.x = speed
+		if not dead:
+			if Input.is_action_pressed("ability1"):
+				emit_signal("ability", 1)
+			if Input.is_action_just_pressed("ability2"):
+				emit_signal("ability", 2)
+			if Input.is_action_just_pressed("ability3"):
+				emit_signal("ability", 3)
+>>>>>>> Stashed changes
 		velocity = move_and_slide(velocity)
 		rpc_unreliable("update_position", position)
-	
+		
+		#TOGGLES DEATHSCREEN WHEN PLAYER IS DEAD
+		if health <= 0:
+			$"/root/Game/CanvasLayer/DeathScreen".show()
+			rpc("die")
+		#KILL PLAYER - remove in future
+		if Input.is_key_pressed(KEY_K):
+			health = 0
 remotesync func update_health(change):
 	health=health+change
 	$HealthBar.scale.x= float(health)/100.0
@@ -48,3 +78,8 @@ remotesync func update_health(change):
 		queue_free()
 remote func update_position(pos):
 	position = pos
+#TURNS PLAYER INTO A "GHOST" AT DEATH
+remote func die():
+	speed = 1500
+	visible = false
+	dead = true
